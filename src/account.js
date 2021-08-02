eee1382d3f01837412cf19be7d524ba5e9edaf1a
Frank Mckenna <fmckenna56330@gmail.com>
2021-08-02 17:45:07 +0100

    every credit transaction comes with a date

--- src/account.js
+++ src/account.js
@@ -1,6 +1,21 @@
 class Account {
   constructor() {
     this.balance = 0;
+    this.date = new Date();
+    this.transactions = [];
+  }
+
+  credit(amount) {
+    this.balance += amount;
+    this.transactions.push(`${this.transactionDate()} || || ${amount} || ${this.balance}`);
+  }
+
+  debit(amount) {
+    this.balance -= amount;
+  }
+
+  transactionDate() {
+    return this.date.toLocaleDateString('en-UK');
   }
 }
 
--- test/account.test.js
+++ test/account.test.js
@@ -2,11 +2,29 @@ const Account = require('../src/account');
 
 describe('Account', () => {
   let account;
+  let date;
   beforeEach(() => {
+    date = new Date();
     account = new Account();
   });
 
   test('account has a starting balance of zero', () => {
     expect(account.balance).toEqual(0);
+  });
+
+  test('credit my account with £500', () => {
+    account.credit(500);
+    expect(account.balance).toEqual(500);
+  });
+
+  test('withdraw £200 from my account', () => {
+    account.credit(500);
+    account.debit(200);
+    expect(account.balance).toEqual(300);
+  });
+
+  test('i want a date with every transation i make', () => {
+    account.credit(500);
+    expect(account.transactions[0]).toEqual(`${date.toLocaleDateString('en-UK')} || || 500 || 500`);
   });
 });
